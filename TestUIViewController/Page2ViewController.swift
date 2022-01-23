9aab65b34dffd296b3bc4d7467f7d8a606d9c991
Huang Grace <huang.yahui.grace@gmail.com>
2022-01-23 10:58:17 +0800

    Timer implement

--- TestUIViewController/Page2ViewController.swift
+++ TestUIViewController/Page2ViewController.swift
@@ -8,7 +8,14 @@
 import UIKit
 
 class Page2ViewController: UIViewController {
-
+    var counter = 0
+    var myTimer: Timer!
+    
+    override func loadView() {
+        super.loadView()
+        
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -17,13 +24,22 @@ class Page2ViewController: UIViewController {
         let view1 = UIView(frame: CGRect(x: 100, y: 100, width: 100, height: 100))
         view1.backgroundColor = UIColor.red
         self.view.addSubview(view1)
+        
+        myTimer = Timer.scheduledTimer(timeInterval: 5, target: self, selector: #selector(setCounter), userInfo: "Grace", repeats: true)
+    }
+    
+    @objc func setCounter() {
+        counter += 1
+        print("counter is \(counter)")
     }
     
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(animated)
         
+        print("Page2ViewController viewDidAppear()")
+        
         let view1 = UIView(frame: CGRect(x: 100, y: 200, width: 100, height: 100))
-        view1.backgroundColor = UIColor.yellow
+        view1.backgroundColor = UIColor.blue
         self.view.addSubview(view1)
     }
 
