ab64073f4b10b87a0934a01181b013c573086f90
Huang Grace <huang.yahui.grace@gmail.com>
2022-01-23 11:32:59 +0800

    Timer rotate rectangle

--- TestUIViewController/Page2ViewController.swift
+++ TestUIViewController/Page2ViewController.swift
@@ -8,7 +8,7 @@
 import UIKit
 
 class Page2ViewController: UIViewController {
-    var counter = 0
+    var counter = 0.0
     var myTimer: Timer!
     
     override func loadView() {
@@ -23,34 +23,24 @@ class Page2ViewController: UIViewController {
         
         let view1 = UIView(frame: CGRect(x: 100, y: 100, width: 100, height: 100))
         view1.backgroundColor = UIColor.red
+        view1.center = self.view.center
         self.view.addSubview(view1)
         
-        myTimer = Timer.scheduledTimer(timeInterval: 5, target: self, selector: #selector(setCounter), userInfo: "Grace", repeats: true)
+        myTimer = Timer.scheduledTimer(withTimeInterval: 0.05, repeats: true, block: { Timer in
+            self.setCounter(sender: view1)
+        })
     }
     
-    @objc func setCounter() {
-        counter += 1
-        print("counter is \(counter)")
+    func setCounter(sender: UIView) {
+        let angle = counter * Double.pi / 180.0
+        sender.transform = CGAffineTransform(rotationAngle: angle)
+        counter += 5
+        print("Now counter is \(counter)")
     }
     
-    override func viewDidAppear(_ animated: Bool) {
-        super.viewDidAppear(animated)
-        
-        print("Page2ViewController viewDidAppear()")
-        
-        let view1 = UIView(frame: CGRect(x: 100, y: 200, width: 100, height: 100))
-        view1.backgroundColor = UIColor.blue
-        self.view.addSubview(view1)
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        print("in viewWillDisappear()")
+        myTimer.invalidate()
     }
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
